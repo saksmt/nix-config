524711cdeb32b199f1c3ac38bfec9e5ceb889ceb
Kirill Saksin <smt@smt.run>
2018-11-11 01:47:11 +0300

    Added transmission daemon

--- apps/media-server.nix
+++ apps/media-server.nix
@@ -4,4 +4,21 @@ with (import ../lib/env-functions.nix);
 whenMediaServer {
   services.plex.enable = true;
   services.nfs.enable = true;
+  services.transmission.enabled = true;
+  services.transmission.settings = {
+    block-list-enabled = true;
+    block-list-url = "http://list.iblocklist.com/?list=bt_level1&fileformat=p2p&archiveformat=gz";
+    dht-enabled = true;
+    download-dir = "/data";
+    lpd-enabled = true;
+    pex-enabled = true;
+    ratio-limit-enabled = true;
+    ratio-limit = 2;
+    rpc-enabled = true;
+    rpc-url = "/transmission/";
+    rpc-port = 9091;
+    rpc-white-list = "127.0.0.1,192.168.*.*";
+    rpc-white-list-enabled = true;
+    utp-enabled = true;
+  };
 }
