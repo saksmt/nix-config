53e28becadcab3f573f7b9250ef4f52ecae4645d
Kirill Saksin <k.saksin@tinkoff.ru>
2018-11-21 10:19:46 +0300

    torrents port

--- apps/media-server.nix
+++ apps/media-server.nix
@@ -26,5 +26,6 @@ whenMediaServer {
   programs.java.package = pkgs.oraclejre;
   programs.java.enable = true;
 
-  networking.firewall.allowedTCPPorts = [ 22 80 8080 8688 8443 8006 8005 32400 9091 ];
+  networking.firewall.allowedTCPPorts = [ 22 80 8080 8688 8443 8006 8005 32400 9091 51413 ];
+  networking.firewall.allowedUDPPorts = [ 51413 ];
 }
