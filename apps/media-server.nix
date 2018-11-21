cca9f01f15b6381a971fc348a99694f5304dcb33
Kirill Saksin <k.saksin@tinkoff.ru>
2018-11-21 11:10:22 +0300

    nfs ports

--- apps/media-server.nix
+++ apps/media-server.nix
@@ -35,8 +35,8 @@ whenMediaServer {
   programs.java.package = pkgs.oraclejre;
   programs.java.enable = true;
 
-  networking.firewall.allowedTCPPorts = [ 22 80 8080 8688 8443 8006 8005 32400 9091 4000 4001 4002 ];
-  networking.firewall.allowedUDPPorts = [ 4000 4001 4002 ];
+  networking.firewall.allowedTCPPorts = [ 22 80 8080 8688 8443 8006 8005 32400 9091 4000 4001 4002 111 ];
+  networking.firewall.allowedUDPPorts = [ 4000 4001 4002 111 ];
 
   networking.firewall.allowedTCPPortRanges = [ { from = 10000; to = 65535; } ];
   networking.firewall.allowedUDPPortRanges = [ { from = 10000; to = 65535; } ];
