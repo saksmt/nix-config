abaa8124ce0f40eddf06e99d186eacc695b298f8
Kirill Saksin <smt@smt.run>
2018-11-27 11:56:51 +0300

    ftp ports on firewall

--- apps/media-server.nix
+++ apps/media-server.nix
@@ -41,7 +41,7 @@ whenMediaServer {
   programs.java.package = pkgs.oraclejre;
   programs.java.enable = true;
 
-  networking.firewall.allowedTCPPorts = [ 22 80 8080 8688 8443 8006 8005 32400 9091 4000 4001 4002 111 2049 ];
+  networking.firewall.allowedTCPPorts = [ 22 21 20 80 8080 8688 8443 8006 8005 32400 9091 4000 4001 4002 111 2049 ];
   networking.firewall.allowedUDPPorts = [ 4000 4001 4002 111 2049 ];
 
   networking.firewall.allowedTCPPortRanges = [ { from = 10000; to = 65535; } ];
