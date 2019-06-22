ad13b571dc2553ae3f630d78823a740f8f629852
Kirill Saksin <smt@smt.run>
2019-06-22 15:26:03 +0300

    attempt to use fresh plex

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -1,12 +1,13 @@
 { config, pkgs }: {
     nixpkgs.overlays = [( self: super: {
+        with super.lib;
         plex = super.plex.overrideAttrs(old: rec {
             name = "plex-${version}";
             version = "1.16.0.1226";
             vsnHash = "7eb2c8f6f";
             sha256 = "8ee806f35ccedcecd0cab028bbe1f7e2ac7de24292b715978d3165c4712f5c40";
 
-            src = super.fetchurl {
+            src = fetchurl {
               url = "https://downloads.plex.tv/plex-media-server/${version}-${vsnHash}/plexmediaserver-${version}-${vsnHash}.x86_64.rpm";
               inherit sha256;
             };
