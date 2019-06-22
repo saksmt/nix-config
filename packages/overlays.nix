cbeeebcd18c03d765aac39b5030c8a91d324b603
Kirill Saksin <smt@smt.run>
2019-06-22 15:36:04 +0300

    attempt to use fresh plex

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -7,7 +7,7 @@
             sha256 = "8ee806f35ccedcecd0cab028bbe1f7e2ac7de24292b715978d3165c4712f5c40";
 
             src = super.fetchurl {
-              url = "https://downloads.plex.tv/plex-media-server-new/${version}-${vsnHash}/plexmediaserver-${version}-${vsnHash}.x86_64.rpm";
+              url = "https://downloads.plex.tv/plex-media-server-new/${version}-${vsnHash}/redhat/plexmediaserver-${version}-${vsnHash}.x86_64.rpm";
               inherit sha256;
             };
         });
