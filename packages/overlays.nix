18ad79699bf00075bdc69dd55ae37bc9a5daaec6
Kirill Saksin <smt@smt.run>
2019-06-22 15:31:32 +0300

    attempt to use fresh plex

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -6,7 +6,7 @@
             vsnHash = "7eb2c8f6f";
             sha256 = "8ee806f35ccedcecd0cab028bbe1f7e2ac7de24292b715978d3165c4712f5c40";
 
-            src = fetchurl {
+            src = super.fetchurl {
               url = "https://downloads.plex.tv/plex-media-server/${version}-${vsnHash}/plexmediaserver-${version}-${vsnHash}.x86_64.rpm";
               inherit sha256;
             };
