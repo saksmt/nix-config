ea50898cb0715530ca2cb8ab90f6e0d2cfab0306
Kirill Saksin <smt@saksmt.dev>
2026-09-14 19:19:02 +0400

    missing catppuccin modules in mac

--- lib/nix-polyfils/src/catppuccin.module.nix
+++ lib/nix-polyfils/src/catppuccin.module.nix
@@ -21,4 +21,23 @@
     { config, lib, ... }:
     {
     };
+
+  darwinModule =
+    { lib, ... }:
+    let
+      todoFlag =
+        name: with lib; {
+          "${name}" = {
+            enable = mkOption {
+              type = types.bool;
+              apply = v: warnIf v "catppuccin for ${name} is not supported as a darwin module yet!" v;
+              default = false;
+            };
+          };
+        };
+    in
+    {
+      options.catppuccin =
+        todoFlag "btop" // todoFlag "bat" // todoFlag "fzf" // todoFlag "delta" // todoFlag "lazygit";
+    };
 }
